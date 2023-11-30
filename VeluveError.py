c561455576e693f14b9ef808df6ec42d268b5948
aycbph <149017003+aycbph@users.noreply.github.com>
2023-11-30 18:20:02 +0100

    Delete VeluveError.py

--- VeluveError.py
+++
@@ -1,8 +0,0 @@
-inputresult = int(input('Írj be egy számot:'))
-
-if not (isinstance(inputresult, int)):
-    raise TypeError('Nem szám típusú amit beírtál')
-else:
-    if inputresult<10:
-        raise ValueError('10-nél nagyobb számot adj meg')
-
